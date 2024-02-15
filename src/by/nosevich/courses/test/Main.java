2a1022051525ec4e0c0d19319211dcd1da16976e
Artem <artem.nosevich@bk.ru>
2024-02-15 21:19:21 +0300

    Edit main class

--- src/by/nosevich/courses/test/Main.java
+++ src/by/nosevich/courses/test/Main.java
@@ -6,12 +6,13 @@ import java.time.temporal.TemporalAccessor;
 
 public class Main {
 
+    public String var;
+
     public static void main(String[] args) {
         DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        String date = "16.05.2029 21:39:48";
+        String date = "16.05.2029 21:39:49";
         TemporalAccessor temporalAccessor = dateTimeFormatter.parse(date);
         LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
         System.out.println(localDateTime);
     }
-
 }
