b1c94fb1c3a415ee4349638b495ee3828fb68ab1
Artem <artem.nosevich@bk.ru>
2024-02-15 21:20:42 +0300

    Clear main class

--- src/by/nosevich/courses/test/Main.java
+++ src/by/nosevich/courses/test/Main.java
@@ -1,18 +1,10 @@
 package by.nosevich.courses.test;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAccessor;
-
 public class Main {
 
-    public String var;
+
 
     public static void main(String[] args) {
-        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
-        String date = "16.05.2029 21:39:49";
-        TemporalAccessor temporalAccessor = dateTimeFormatter.parse(date);
-        LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
-        System.out.println(localDateTime);
+
     }
 }
