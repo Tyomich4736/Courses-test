7795217e1ea2bef8bdc32ef3dfcd2e8aaec5ccd2
Artem <artem.nosevich@bk.ru>
2024-02-15 21:13:16 +0300

    Add main logic

---
+++ src/by/nosevich/courses/test/Main.java
@@ -0,0 +1,17 @@
+package by.nosevich.courses.test;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+public class Main {
+
+    public static void main(String[] args) {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+        String date = "16.05.2029 21:39:48";
+        TemporalAccessor temporalAccessor = dateTimeFormatter.parse(date);
+        LocalDateTime localDateTime = LocalDateTime.from(temporalAccessor);
+        System.out.println(localDateTime);
+    }
+
+}
